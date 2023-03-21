ff767f94f9ad81b678487e158e8fcfb834911f19
Geran1ka <roman.khoruzhy@gmail.com>
2023-03-21 08:50:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const product = 'sony playstation';
+
+const count = 5;
+
+const category = 'Игровая приставка';
+
+const price = 20000;
+
+console.log('product: ', product);
+
+console.log(`Итоговая сумма покупки ${product} составляет ${count * price} руб.`)
