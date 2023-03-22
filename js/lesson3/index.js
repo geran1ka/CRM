ef7bfcc7711a912a8c751e0f56b1885679016bd0
geran1ka <roman.khoruzhy@gmail.com>
2023-03-22 22:48:23 +0300

    lession 4.3

--- js/lesson3/index.js
+++ js/lesson3/index.js
@@ -1,13 +1,17 @@
 'use strict';
 
+//Задача №1
+
 const product = prompt('Введите наименование товара: ');
 const count = +prompt('Введите количество товара: ');
 const category = prompt('Введите категорию товара: ');
 const price = +prompt('Введите цену товара: ');
 
+if (Number.isNaN(count) || Number.isNaN(price)) {
+  console.log("Вы ввели некоректные данные");
+} else {
+  console.log(`"На складе ${count} единиц(ы) товара "${product}" на сумму ${count * price} руб."`);
+}
 
-console.log('count: ', typeof count);
-console.log('price: ', typeof price);
-console.log(`"На складе ${count} единиц(ы) товара "${product}" на сумму ${count * price} руб."`)
 
 
---
+++ js/lesson3/taskHard_2.js
@@ -0,0 +1,16 @@
+/*
+const income = +prompt('Введите сумму Вашего дохода: ');
+let nalog = NaN;
+
+if (Number.isNaN(income)) {
+  console.log(`Вы ввели некорректные данные!`);
+} else if (income <= 15000) {
+  nalog = income * 0.13;
+} else if (income > 15000 && income <= 50000) {
+  nalog = 15000 * 0.13 + (income - 15000) * 0.2;
+} else {
+  nalog = 15000 * 0.13 + 35000 * 0.2 + (income - 50000) * 0.3;
+}
+
+console.log(`Вам необходимо заплатить налог в размере: ${nalog}р`);
+*/
