3aa73ad75b951dd4536447d2b479f7dfb7e71b19
Geran1ka <roman.khoruzhy@gmail.com>
2023-03-24 16:49:37 +0300

    Решение 4 задачи от Кирилла

--- js/lesson4/hard_task_02.js
+++ js/lesson4/hard_task_02.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const getMinNumber = (a, b) => {
   a > b || a;
   a < b || (a = b);
@@ -11,8 +11,12 @@ const getMaxNumber = (a, b) => {
   a > b || (a = b);
   return a;
 }
+*/
+const getMinNumber = (a, b) => (a > b) * b + (a < b) * a;
 
-const minimun = getMinNumber(150, 25);
+const getMaxNumber = (a, b) => (a > b) * a + (a < b) * b;
+
+const minimun = getMinNumber(-150, 25);
 console.log('minimun: ', minimun);
 
 const maximum = getMaxNumber(150, 25);
