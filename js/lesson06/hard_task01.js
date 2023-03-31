406717110e381d4a612b1854415cb070e6a4b256
geran1ka <roman.khoruzhy@gmail.com>
2023-03-31 21:38:43 +0300

    eslint hard task

--- js/lesson06/hard_task01.js
+++ js/lesson06/hard_task01.js
@@ -1,20 +1,22 @@
 'use strict';
 
 const allCashbox = [
-  [12, 4500], 
-  [7, 3210], 
-  [4, 650], 
-  [3, 1250], 
-  [9, 7830], 
-  [1, 990], 
-  [6, 13900], 
-  [1, 370]
+  [12, 4500],
+  [7, 3210],
+  [4, 650],
+  [3, 1250],
+  [9, 7830],
+  [1, 990],
+  [6, 13900],
+  [1, 370],
 ];
 
 const getAveragePriceGoodsAlt = (arr) => {
-  const newArr = arr.reduce((acc, current) =>  [acc[0] + current[0], acc[1] + current[1]]);
+  const newArr = arr.reduce(([totalCount, totalCost], [count, cost]) =>
+    [totalCount + count, totalCost + cost]);
 
-  return newArr.reduce((acc, item) => Math.round(item / acc));
+  return newArr.reduce((totalCount, totalCost) =>
+    Math.round(totalCost / totalCount));
 };
 
 const resultAlt = getAveragePriceGoodsAlt(allCashbox);
