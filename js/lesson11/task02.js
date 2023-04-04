592e86cf0d301a6a226b66e74efe8c4b0b2e8934
geran1ka <roman.khoruzhy@gmail.com>
2023-04-04 23:22:56 +0300

    Элегантное решение от Кирилла

--- js/lesson11/task02.js
+++ js/lesson11/task02.js
@@ -1,14 +1,12 @@
 'use strict';
 const arrayNumber = [];
+
 const randomNumber = () => Math.round(Math.random() * 10);
-const getArrayRandomNumber = (arr) => {
-  const array = [...arr];
-  const sum = array.reduce((acc, item) => acc + item, 0);
-  if (sum < 50) {
-    array.push(randomNumber());
-    return getArrayRandomNumber(array);
-  }
-  return array;
+
+const getArrayRandomNumber = arr => {
+  if (arr.reduce((acc, item) => acc + item, 0) >= 50) return [...arr];
+
+  return getArrayRandomNumber([...arr, randomNumber()]);
 };
 
 const result = getArrayRandomNumber(arrayNumber);
